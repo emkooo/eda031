97f08c2bc25b81f1ac3f319485d8644cea6043f9
Emir Husic <emir.husic@gmx.com>
2016-02-07 20:23:47 +0100

    Still stuff to do

--- lab2/wordCreater.cc
+++ lab2/wordCreater.cc
@@ -6,33 +6,73 @@ a new file with all the words.*/
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
+#include <vector>
 
+using namespace std;
 
 int main(){
 
-string line;
-ifstream myInputFile("/usr/share/dict/words");
-ofstream myOutputFile("dictionary.txt");
-if(!myInputFile)
+string line;		//String with current line
+
+
+ifstream myInputFile("/usr/share/dict/words");	// get the words
+ofstream myOutputFile("words.txt");		// Insert words here
+
+if(!myInputFile) //If we cant find the file 
 {
 	cout << "Damaged or not found." << endl;
 }
 else{
 	cout << "File found and okay!" << endl;
-	while(getline(myInputFile,line)){
-		myOutputFile << line << endl;
-	}
 
-
-	/*
+		/*
 	Read next line.
 	save the word
 	into new file.
 
 	*/
-}
-myInputFile.close();
+unsigned currentPosition = 0;
+char myCharVector[3];
+
+	while(getline(myInputFile,line)){		//Keep getting the words.
+
+		if(line.size() > 3)		//If we've got possibilie of triagram
+		{
+			//cout << "we've got triagram word" << endl;
+
+			while(currentPosition < line.size() - 2)
+			{
+				for(int i = 0; i <= 2; i++)
+				{
+						myCharVector[i] = line.at(i + currentPosition);
+
+				}
+				// myCharVector[currentPosition] = line.at(currentPosition);
+				// myCharVector[currentPosition + 1] = line.at(currentPosition + 1);
+				// myCharVector[currentPosition + 2] = line.at(currentPosition + 2);
+
+				currentPosition++;
+			}
+
+		}
+
+		myOutputFile << line << " " << line.size() <<  " " << myCharVector[0] << myCharVector[1] << myCharVector[2]<< endl;
+
+		memset(&myCharVector[0], 0, sizeof(myCharVector)); //Clear array
+		currentPosition = 0;		//Reset positioner.
+		
+	}
+
+
+/*	 Also need to check how many triagrams it contains,
+	Add how many triagrams,
+	 Add triagram in alphabetical order.
+	 No uppercase letters.
+	 Example: hand 2 and hand
+	  */
+myInputFile.close();		//Close the 2 used files.
 myOutputFile.close();
+}
+
 	
 }
