767bddddd4fa821d07465029d58a861d79a81da0
Emir Husic <emir.husic@gmx.com>
2016-02-07 21:06:32 +0100

    Must convert to small letters

--- lab2/wordCreater.cc
+++ lab2/wordCreater.cc
@@ -33,43 +33,53 @@ else{
 	*/
 unsigned currentPosition = 0;
 char myCharVector[3];
+vector<string> myStringVector;
 
 	while(getline(myInputFile,line)){		//Keep getting the words.
 
 		if(line.size() > 3)		//If we've got possibilie of triagram
 		{
+			string s;
 			//cout << "we've got triagram word" << endl;
 
 			while(currentPosition < line.size() - 2)
 			{
+				s = "";
 				for(int i = 0; i <= 2; i++)
 				{
 						myCharVector[i] = line.at(i + currentPosition);
+						s = s + myCharVector[i];
 
 				}
+				myStringVector.push_back(s);
+				
+
 				// myCharVector[currentPosition] = line.at(currentPosition);
 				// myCharVector[currentPosition + 1] = line.at(currentPosition + 1);
 				// myCharVector[currentPosition + 2] = line.at(currentPosition + 2);
 
 				currentPosition++;
 			}
+			sort(myStringVector.begin(),myStringVector.end());			//Sorts the vector
 
 		}
 
-		myOutputFile << line << " " << line.size() <<  " " << myCharVector[0] << myCharVector[1] << myCharVector[2]<< endl;
+		myOutputFile << line << " " << myStringVector.size();
+		for(int i =0; i<myStringVector.size(); i++)
+		{
+			string apa = myStringVector[i];
+			myOutputFile << " " << apa;				//Remove from the last item?!
+
+		}
+		myOutputFile << endl;
 
 		memset(&myCharVector[0], 0, sizeof(myCharVector)); //Clear array
 		currentPosition = 0;		//Reset positioner.
+		myStringVector.clear();
 		
 	}
 
 
-/*	 Also need to check how many triagrams it contains,
-	Add how many triagrams,
-	 Add triagram in alphabetical order.
-	 No uppercase letters.
-	 Example: hand 2 and hand
-	  */
 myInputFile.close();		//Close the 2 used files.
 myOutputFile.close();
 }
