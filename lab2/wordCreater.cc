aafffeb0ce59b000274ddfe2c43b1dfa87525563
Emir Husic <emir.husic@gmx.com>
2016-02-07 17:53:47 +0100

    Edited wordCreater

--- lab2/wordCreater.cc
+++ lab2/wordCreater.cc
@@ -5,16 +5,41 @@ a new file with all the words.*/
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 
 int main(){
-	ofstream myFile;		
-	myFile.open(/usr/share/dict/words); // not sure..
 
-	if(myFile.is_open())	//check that there indeed is a file
-	{
-		cout << "ev thing ok" << endl;
+string line;
+ifstream myInputFile("/usr/share/dict/words");
+ofstream myOutputFile("dictionary.txt");
+if(!myInputFile)
+{
+	cout << "Damaged or not found." << endl;
+}
+else{
+	cout << "File found and okay!" << endl;
+	while(getline(myInputFile,line)){
+		myOutputFile << line << endl;
+	}
 
-	}		
+
+	/*
+	Read next line.
+	save the word
+	into new file.
+
+	*/
+}
+myInputFile.close();
+myOutputFile.close();
+	//ofstream myFile;		
+	//myFile.open(/usr/share/dict/words); // not sure..
+
+	// if(myFile.is_open())	//check that there indeed is a file
+	// {
+	// 	cout << "ev thing ok" << endl;
+
+	// }		
 }
