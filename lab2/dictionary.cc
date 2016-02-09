8616d519a2d509e53bd69e8a34e564f22a0d5b59
Emir Husic <emir.husic@gmx.com>
2016-02-09 17:00:13 +0100

    add_triagram_suggestions (DONE)

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -3,69 +3,122 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <sstream>
 #include "word.h"
 #include "dictionary.h"
 #include <unordered_set>
 
 using namespace std;
 
-Dictionary::Dictionary() {
-	//unordered_set<string> mySet;		//Words are going to be stored here.
-	//mySet;
-
-	// ofstream myFile;
-	// myFile.open("words.txt");
-
+Dictionary::Dictionary() 
+{
 	ifstream myFile("words.txt");		//Read our .txt file
 
 	if(!myFile)		//if file was not found
 	{
 		cout << "File damaged or not found" << endl;
 		
-
 	}
 	else
 	{
 		cout << "File okay!" << endl;
 
-		// string lol;
-		// getline(myFile,lol);
-		// cin >> lol;
-		// cout << "the first thing we see is = " <<  lol << endl;
-
 		string line;
-
-		/* Problematical since we get the whole line, we only want first string */
 		while(getline(myFile,line))		//Gets the line.
-		{
-			getline(myFile,line, ' ');	//Gets the string until 1st whitespace.
+		{			
+		vector<string> ourWords;
+		istringstream split(line);
+		for(string each; getline(split,each, ' '); ourWords.push_back(each))
+			;
+				string theWord = ourWords[0];
 
-			//cin >> line;
-			mySet.insert(line);			//Inser that word into the set
-		}
+				mySet.insert(theWord);
 
-myFile.close();		//close the file
-	
-	
+				if(theWord.size() < MAXWORDLENGTH)
+				{
+				 	vector<string> theTriagram (stoi(ourWords[1]));
+
+						for(unsigned int i = 2; i < ourWords.size(); i++)
+						{
+							theTriagram[i - 2] = ourWords[i];
+						}						
+							Word vE(theWord,theTriagram);
+							words[theWord.size()].push_back(vE);
+				}
+			}
+								myFile.close();		//close the file	
+	}
 }
 
-}
-
-bool Dictionary::contains(const string& word) const {
-
-	///unordered_set<string> mySet;
+bool Dictionary::contains(const string& word) const 
+{
 
 	unordered_set<string>::const_iterator got = mySet.find(word);
-	if(got == mySet.end()){
-		return false;
-	}
-	return true;
+		if(got == mySet.end())
+		{
+			return false;
+		}
+			return true;
 
-
-	return true;
 }
 
-vector<string> Dictionary::get_suggestions(const string& word) const {
-	vector<string> suggestions;
+void Dictionary::add_trigram_suggestions(vector<string>& t, const string& word) const
+{
+	int stringSize = word.size();		// get size of string
+	int currentPosition = 0;
+	string s = word;
+	vector<string> myStringVector;
+ 
+	if(stringSize > 3)
+	{
+		char myCharVector[3];
+		while(currentPosition < stringSize - 2)
+		{			
+			string temps = "";
+			for(int i = 0; i <= 2; i++)
+			{
+					myCharVector[i] = s.at(i + currentPosition);
+					temps = temps + myCharVector[i];
+			}
+			myStringVector.push_back(temps);
+
+			currentPosition++;
+		}
+
+		sort(myStringVector.begin(),myStringVector.end());			//Sorts the vector
+
+		for(int j = stringSize -1; j <= stringSize + 1; j++)
+			{	cout << " stringSize is " << stringSize << endl;
+					for(unsigned int k = 0; k< words[j].size() - 1; k++)
+					{
+					
+						if(words[j][k].get_matches(myStringVector) >= myStringVector.size()/2)
+						{ 		
+							t.push_back(words[j][k].get_word());
+						}
+					}
+			}
+	}
+	
+else{
+		vector<Word> tempOne = words[stringSize];
+			for(unsigned int a = 0 ; a < tempOne.size(); a++)
+			{
+				t.push_back(tempOne[a].get_word());
+
+			}
+
+	}
+			sort(t.begin(),t.end());
+}
+
+
+vector<string> Dictionary::get_suggestions(const string& word) const 
+{
+		vector<string> suggestions;
+		add_trigram_suggestions(suggestions,word);
+
+
+
 	return suggestions;
 }
--- lab2/dictionary.h
+++ lab2/dictionary.h
@@ -1,9 +1,11 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
+#define MAXWORDLENGTH 25
 
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include "word.h"
 
 using namespace std;
 
@@ -12,8 +14,14 @@ public:
 	Dictionary();
 	bool contains(const std::string& word) const;
 	std::vector<std::string> get_suggestions(const std::string& word) const;
+//	void rank_suggestions(vector<string>& t);
+	void add_trigram_suggestions(vector<string>& t, const string& word) const;
+
 private:
 	unordered_set<string> mySet;
+     vector<Word> words[MAXWORDLENGTH]; // words[i] = the words with i letters,
+                                		// ignore words longer than 25 letters
+
 };
 
 #endif
