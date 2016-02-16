22b0097e0080130df20ef3fe6a10ecaa83974dab
johannesnilsson <johannesnilsson2@gmail.com>
2016-02-16 12:05:06 +0100

    A3 nästan klar

--- lab3/date.cc
+++ lab3/date.cc
@@ -1,6 +1,55 @@
 #include <ctime>  // time and localtime
 #include "date.h"
 
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& os, const Date& d)
+{
+    // write obj to stream
+
+    //cout << date på detta objekt skall:
+    //outputta innehållet, "dates"
+    //på formatet yyyy-mm-dd '\n'
+	
+		os << d.getYear() << "-";
+
+		if(d.getMonth() < 10)
+		{
+			os << 0 << d.getMonth() << "-";
+		}
+		else
+		{
+			os << d.getMonth();
+		}
+
+		if(d.getDay() < 10)
+		{
+			os << 0 << d.getDay();
+		}
+		else
+		{
+			os << d.getDay();
+		}
+	
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Date& d)
+{
+    // read obj from stream
+    //id d inte finns tillåt inte input operationer.
+    //detta görs när man sätter stream state till failbit
+   // Date* dateptr = d; 
+    //if( dateptr == nullptr )
+   // {
+    //    is.setstate(std::ios::failbit);
+   // }
+
+    is >> d.year >> d.month >> d.day;
+
+    return is;
+}
+
 int Date::daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 Date::Date() {
@@ -11,20 +60,111 @@ Date::Date() {
 	day = locTime->tm_mday;
 }
 
-Date::Date(int y, int m, int d) {}
+Date::Date(int y, int m, int d) {
+	year = y;
+	month = m;
+	day = d; 
+}
 
 int Date::getYear() const {
-	return 0;
+	return year;
 }
 
 int Date::getMonth() const {
-	return 0;
+	return month;
 }
 
 int Date::getDay() const {
-	return 0;
+	return day;
 }
 
 void Date::next() {
+	day = day + 1;
+	switch(month)
+	{
+		case 1:
+		if (day > 31){
+			day = 1;
+			month = month + 1;
+		}
+		break;
+		
+		case 2:
+		if(day > 28){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 3:
+		if(day > 31){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 4:
+		if(day > 30){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 5:
+		if(day > 31){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 6:
+		if(day > 30){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 7:
+		if(day > 31){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 8:
+		if(day > 31){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 9:
+		if(day > 30){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 10:
+		if(day > 31){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 11:
+		if(day > 30){
+			day = 1;
+			month = month + 1;
+		} 
+		break;
+		
+		case 12:
+		if(day > 31){
+			day = 1;
+			month = 1;
+			year = year + 1;
+		} 
+		break;	
+	}
 }
-
--- lab3/date.h
+++ lab3/date.h
@@ -1,6 +1,8 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <iostream>
+
 class Date {
 public:
 	Date();                    // today's date
@@ -9,11 +11,16 @@ public:
 	int getMonth() const;      // get the month
 	int getDay() const;        // get the day
 	void next();               // advance to next day
+	friend std::ostream& operator<<(std::ostream& os, const Date& d);
+	friend std::istream& operator>>(std::istream& is, const Date& d);
+
 private:
 	int year;  // the year (four digits)
 	int month; // the month (1-12)
 	int day;   // the day (1-..)
 	static int daysPerMonth[12]; // number of days in each month
 };
+
+
 
 #endif
