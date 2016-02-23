ce1720ac6f583a7157e3511658aca707ef8644e0
Emir Husic <emir.husic@gmx.com>
2016-02-23 15:11:33 +0100

    A1 - Done

--- lab4/nameserver/hns.cc
+++ lab4/nameserver/hns.cc
@@ -5,22 +5,44 @@
 **************************************************/
 
 #include "hns.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-
+using namespace std;
 void 
-HNS::insert(const HostName&, const IPAddress&)
-{
-
+HNS::insert(const HostName& hostAddr, const IPAddress& ipAddr)
+{ 
+	size_t hostHashValue = host_hash(hostAddr) % size;
+	IPContainer[hostHashValue].emplace_back(hostAddr,ipAddr);
 }
 
 bool 
-HNS::remove(const HostName&)
+HNS::remove(const HostName& hostAddr)
 {
-	return 0;
+	size_t hostHashValue = host_hash(hostAddr) % size;
+	std::vector<std::pair<HostName,IPAddress>> v = IPContainer[hostHashValue];
+	
+	for(size_t i = 0; i < v.size(); i++)
+	{
+		if(v.at(i).first == hostAddr)
+		{
+			v.erase(v.begin() + i);
+			return true;	
+		}  
+	}
+	return false;
 }
 
 IPAddress 
-HNS::lookup(const HostName&) const
+HNS::lookup(const HostName& hostAddr) const
 {
-	return 0;
+	size_t hostHashValue = host_hash(hostAddr) % size;
+	std::vector<std::pair<HostName,IPAddress>> v = IPContainer[hostHashValue];
+
+	for(size_t i = 0; i < v.size(); i++)
+	{
+		if(v.at(i).first == hostAddr) return v.at(i).second;
+	}
+	return NON_EXISTING_ADDRESS;
 }
--- lab4/nameserver/hns.h
+++ lab4/nameserver/hns.h
@@ -2,18 +2,21 @@
 #define HNS_H
 
 #include "nameserverinterface.h"
+#include <vector>
 
 class HNS : public virtual NameServerInterface {
 public:
-	HNS(unsigned long hashMapSize) : size(hashMapSize){};
+	HNS(std::size_t hashMapSize) : size(hashMapSize){ IPContainer.reserve(hashMapSize);};	//Allocate memory 
 	void insert(const HostName&, const IPAddress&) override;
 	bool remove(const HostName&) override;
 	IPAddress lookup(const HostName&) const override;
 
 private:
-	size_t size;
+	std::size_t size;
+	std::hash<std::string> host_hash;
+	std::vector<std::vector<std::pair<HostName,IPAddress>>> IPContainer;
+	
 };
-
 
 
 #endif
